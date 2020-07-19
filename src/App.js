bf0c45833bb62289b85750a3c3626ed8613b71c2
zboompopz <zboompopz@hotmail.com>
2020-07-19 12:15:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Profie from './components/Profile';
+import Profile from './components/Profile';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Header/>
+    <Home/>
+    <About/>
+    <Contact/>
+    <Profile/>
+    <Footer/>
+       
     </div>
   );
 }
